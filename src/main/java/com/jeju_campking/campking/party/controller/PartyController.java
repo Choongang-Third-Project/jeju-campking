616349dde1b1a414f0e32c46622838889223d9a5
lkdcode <dlrlejr1@naver.com>
2023-05-12 11:23:21 +0900

    :sparkles: TB_PARTY , TB_PARTY_REPLY SQL

--- src/main/java/com/jeju_campking/campking/party/controller/PartyController.java
+++ src/main/java/com/jeju_campking/campking/party/controller/PartyController.java
@@ -1,35 +1,17 @@
 package com.jeju_campking.campking.party.controller;
 
-import com.jeju_campking.campking.party.dto.response.PartyAllListResponseDTO;
 import com.jeju_campking.campking.party.service.PartyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-/**
- * TODO : 파티 모집 게시판의 URL
- *  /parties/all-list/{type}/{sort} : 타입별로, 정렬기준으로 모든 게시판 조회
- */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/parties")
 @Slf4j
 public class PartyController {
     private final PartyService partyService;
-
-    @GetMapping("/all-list/{type}/{sort}")
-    public ResponseEntity<?> findAllBySort(
-            @PathVariable String type,
-            @PathVariable String sort
-    ) {
-        log.info("partyController/findAllBySort : type= {}, sort= {}", type, sort);
-
-        List<PartyAllListResponseDTO> allBySort = partyService.findAllBySort(type, sort);
-
-        return ResponseEntity.ok().body(allBySort);
-    }
 }
 
--- src/main/java/com/jeju_campking/campking/party/service/PartyService.java
+++ src/main/java/com/jeju_campking/campking/party/service/PartyService.java
@@ -1,20 +1,7 @@
 package com.jeju_campking.campking.party.service;
 
-import com.jeju_campking.campking.party.dto.response.PartyAllListResponseDTO;
-import com.jeju_campking.campking.party.repository.PartyMapper;
-import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-@RequiredArgsConstructor
 public class PartyService {
-
-    private final PartyMapper partyMapper;
-
-    public List<PartyAllListResponseDTO> findAllBySort(String type, String sort) {
-        return partyMapper.findAllBySort(type, sort);
-    }
 }
