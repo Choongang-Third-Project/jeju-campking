3baf8eb1477aaaf0c9c6c36256558ef79f52dc63
lkdcode <dlrlejr1@naver.com>
2023-05-14 16:21:29 +0900

    :white_check_mark:  메시지 전체 조회, 보내기 Mapper 테스트 완료

--- src/main/java/com/jeju_campking/campking/party/repository/PartyMessageMapper.java
+++ src/main/java/com/jeju_campking/campking/party/repository/PartyMessageMapper.java
@@ -1,16 +1,25 @@
 package com.jeju_campking.campking.party.repository;
 
-public interface PartyMessageMapper {
+import com.jeju_campking.campking.party.dto.request.PartyMessageRequestDTO;
+import com.jeju_campking.campking.party.entity.PartyMessage;
+import org.apache.ibatis.annotations.Mapper;
 
-    // 메시지 보내기
-    // 보내는 사람, 받는 사람, 내용, 쪽지 번호 등
-    void send();
+import java.util.List;
+
+@Mapper
+public interface PartyMessageMapper {
 
     // 내가 받은 메시지를 모두 조회하는 기능
     // 받은 사람의 아이디로 조회
-    void read();
+    List<PartyMessage> findAll(Long memberId);
+
+
+    // 메시지 보내기
+    // 보내는 사람, 받는 사람, 내용, 쪽지 번호 등
+    boolean send(PartyMessageRequestDTO dto);
 
     // 받은 메시지를 삭제하는 기능
     // 메시지 넘버로 삭제
     void delete();
+
 }
