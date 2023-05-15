71186ad281877be11931aa45b4ce2077b6fca2f3
JOY0987 <jjoyewon@gmail.com>
2023-05-15 19:33:58 +0900

    파티메세지 전체보기, 전송 postman 테스트

--- src/main/java/com/jeju_campking/campking/party/controller/PartyMessageController.java
+++ src/main/java/com/jeju_campking/campking/party/controller/PartyMessageController.java
@@ -27,18 +27,22 @@ public class PartyMessageController {
 
         log.info("partyMessage.findAll.info : {}", memberNumber);
 
-        if (memberNumber == null) {
-            return ResponseEntity.badRequest().body("조회할 멤버의 ID 없음!");
-        }
-
         List<PartyMessage> messageList = partyMessageService.findAll(memberNumber);
 
-        return ResponseEntity.ok().body(messageList);
+        if (messageList == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("조회할 메세지가 없습니다.");
+        }
+
+        return ResponseEntity
+                .ok()
+                .body(messageList);
     }
 
     @PostMapping("/send")
     public ResponseEntity<?> send(
-            PartyMessageRequestDTO dto,
+            @RequestBody PartyMessageRequestDTO dto,
             BindingResult result) {
         log.info("partyMessage.send.info : {}", dto);
 
@@ -49,18 +53,20 @@ public class PartyMessageController {
                     .body(result.toString());
         }
         // 메세지 전송 성공여부 검증
-        // 성공
         try {
-            if (partyMessageService.send(dto)) {
-                return ResponseEntity
-                        .ok()
-                        .body(dto);
+            if (!partyMessageService.send(dto)) {
+               return ResponseEntity
+                       .badRequest()
+                       .body("메세지 전송에 실패했습니다.");
             }
+            return ResponseEntity
+                    .ok()
+                    .body(dto);
         } catch (Exception e) {
             log.warn("partyMessageService.send.warn {}", dto);
+            return ResponseEntity
+                    .internalServerError()
+                    .body(e.getMessage());
         }
-        // 실패
-        return ResponseEntity
-                .internalServerError().body("FAIL");
     }
 }
