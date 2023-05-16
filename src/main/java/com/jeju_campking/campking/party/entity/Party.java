66bfce32b11ffc1545e7f18dbb0276a52807498e
lkdcode <dlrlejr1@naver.com>
2023-05-16 16:21:54 +0900

    :white_check_mark:  partyContent 추가

--- src/main/java/com/jeju_campking/campking/party/entity/Party.java
+++ src/main/java/com/jeju_campking/campking/party/entity/Party.java
@@ -1,10 +1,20 @@
 package com.jeju_campking.campking.party.entity;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Party {
     private Long partyNumber;
     private String partyTitle;
+    private String partyContent;
     private LocalDateTime partyStartDate;
     private LocalDateTime partyEndDate;
     private Long partySize;
