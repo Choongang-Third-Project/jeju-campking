3ddcd50d5c52ac2441da1b402179ac7690ac9685
lkdcode <dlrlejr1@naver.com>
2023-05-18 17:31:31 +0900

    :sparkles:  자동 로그인 인터셉터

--- src/main/java/com/jeju_campking/campking/interceptor/AutoLoginInterceptor.java
+++ src/main/java/com/jeju_campking/campking/interceptor/AutoLoginInterceptor.java
@@ -3,11 +3,9 @@ package com.jeju_campking.campking.interceptor;
 import com.jeju_campking.campking.member.entity.Member;
 import com.jeju_campking.campking.member.repository.MemberMapper;
 import com.jeju_campking.campking.member.service.MemberService;
-import com.jeju_campking.campking.util.LoginUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.Cookie;
@@ -30,23 +28,25 @@ public class AutoLoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 1. 자동 로그인 쿠키를 탐색
 
-        Cookie cookie = WebUtils.getCookie(null,null);
+        Cookie cookie = WebUtils.getCookie(request, AUTO_LOGIN_COOKIE);
 
-        // 2. 쿠키에 저장된 세션 아이디를 읽는다
+        if (cookie != null) {
+            // 2. 쿠키에 저장된 세션 아이디를 읽는다
+            String sessionId = cookie.getValue();
 
+            // 3. DB 에서 세션 아이디로 회원 정보를 조회한다.
+            // SQL 필요
 
+            Member foundMember = memberMapper.findMemberByCookie(sessionId);
 
-        // 3. DB 에서 세션 아이디로 회원 정보를 조회한다.
-        // SQL 필요
+            // 4. 회원이 조회가 되었고 만료일 이전이라면
+            if (foundMember != null && LocalDateTime.now().isBefore(foundMember.getMemberCookieDate())) {
+                // 5. 로그인 처리
+                memberService.maintainLoginState(request.getSession(), foundMember.getMemberEmail());
+            }
 
+        }
 
-
-        // 4. 회원이 조회가 되었고 만료일 이전이라면
-
-
-
-        // 5. 로그인 처리
-        // memberService
 
         return true;
     }
