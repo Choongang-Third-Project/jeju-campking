cabffe3713336cbd88b6b2a93b95cdbf7cf70827
lkdcode <dlrlejr1@naver.com>
2023-05-21 05:08:18 +0900

    :recycle:  Page setter 대신 of 사용

--- src/main/java/com/jeju_campking/campking/board/dto/page/Page.java
+++ src/main/java/com/jeju_campking/campking/board/dto/page/Page.java
@@ -13,13 +13,20 @@ public class Page {
     private int pageNo; // 클라이언트가 보낸 페이지 번호
     private int amount; // 클라이언트가 보낸 목록 게시물 수
 
-    public Page(){
+    public static Page of(int pageNo, int amount) {
+        Page page = new Page();
+        page.setPageNo(pageNo);
+        page.setAmount(amount);
+        return page;
+    }
+
+    public Page() {
         this.pageNo = 1;
         this.amount = 10;
     }
 
-    public void setPageNo(int pageNo){
-        if(pageNo<1 || pageNo > Integer.MAX_VALUE){
+    private void setPageNo(int pageNo) {
+        if (pageNo < 1 || pageNo > Integer.MAX_VALUE) {
             this.pageNo = 1;
             return;
         }
@@ -27,18 +34,17 @@ public class Page {
     }
 
 
-    public void setAmount(int amount){
-        if(amount < 5 || amount > 20){
+    private void setAmount(int amount) {
+        if (amount < 5 || amount > 20) {
             this.amount = 10;
             return;
         }
         this.amount = amount;
     }
 
-    public int getPageStart(){
+    public int getPageStart() {
         return (pageNo - 1) * amount;
     }
-
 
 
 }
