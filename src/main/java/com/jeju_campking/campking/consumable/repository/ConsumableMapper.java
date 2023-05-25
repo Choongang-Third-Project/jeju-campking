b0f9c0bac694d30e1a5e856bff71180e4241e0a4
choyejin <bibianabb@naver.com>
2023-05-25 16:37:44 +0900

    sql 더미 테스트 제목 좀 더 다양하게

--- src/main/java/com/jeju_campking/campking/consumable/repository/ConsumableMapper.java
+++ src/main/java/com/jeju_campking/campking/consumable/repository/ConsumableMapper.java
@@ -11,45 +11,37 @@ import java.util.List;
 create table TB_CONSUMABLE (
 	consumable_idx INT(10) AUTO_INCREMENT,
 	consumable_name VARCHAR(20) NOT NULL,
+	consumable_price INT(10) NOT NULL,
 	consumable_info VARCHAR(200) NOT NULL,
-	consumable_category VARCHAR(30),
-	consumable_wish VARCHAR(20)
-	PRIMARY KEY(consumable_idx),
-	FOREIGN KEY (consumable_category) REFERENCES TB_CATEGORY(consumable_category),
-	FOREIGN KEY (consumable_wish) REFERENCES TB_WISH(consumable_wish)
-);
-
--- 소모품 카고테고리 테이블 생
-CREATE table TB_CATEGORY (
-	consumable_category VARCHAR(10),
-	consumable_img VARCHAR(2000) NOT NULLm
-	consumable_idx INT(10),
-	PRIMARY KEY(consumable_category),
-	FOREIGN KEY (consumable_idx) REFERENCES TB_CONSUMABLE(consumable_idx)
+	consumable_category VARCHAR(30) NOT NULL,
+	consumable_img VARCHAR(2000) NOT NULL,
+	PRIMARY KEY(consumable_idx)
 );
 
 -- 소모품 찜목 테이블 생
-CREATE table TB_WISH(
-	consumable_wish INT(10) AUTO_INCREMENT,
-	consumable_idx INT(10),
-	member_number INT(10),
-	PRIMARY KEY(consumable_wish),
-	FOREIGN KEY (consumable_idx) REFERENCES TB_CONSUMABLE(consumable_idx),
-	FOREIGN KEY (member_number) REFERENCES TB_MEMBER(member_number)
+CREATE table TB_CONSUMABLE_WISH (
+	consumable_wish INT(10) NOT NULL AUTO_INCREMENT,
+	consumable_idx INT(10) NOT NULL,
+	member_number INT(10) NOT NULL,
+	is_like VARCHAR(1) NOT NULL DEFAULT 'N',
+	PRIMARY KEY(consumable_wish)
 );
-
 */
+
 // TODO: 소모품 mapper
 public interface ConsumableMapper {
     //전체 조회 기능
     @Select(
-        "SELECT * FROM TB_CONSUMABLE\n"
+        "SELECT \n" +
+                "consumable_idx,consumable_name,consumable_price,consumable_info,consumable_category,consumable_img \n" +
+                "FROM TB_CONSUMABLE\n"
     )
     List<Consumable> findAll();
 
-    //키워드 검색해서 보여주기
+    //키워드 검색해서 조회하기
     @Select(
             "SELECT \n"+
+                    "consumable_idx,consumable_name,consumable_price,consumable_info,consumable_category,consumable_img \n" +
                     "FROM TB_CONSUMABLE\n" +
                     "WHERE consumable_name LIKE %#{keyword}%"
     )
@@ -61,22 +53,35 @@ public interface ConsumableMapper {
     )
     int allCount();
 
-    //카테고리별 소모품 보여주기
+    //카테고리별 소모품 조회하기
     @Select(
-            "SELECT * FROM TB_CONSUMABLE" +
+            "SELECT \n" +
+                    "consumable_idx,consumable_name,consumable_price,consumable_info,consumable_category,consumable_img \n" +
+                    " FROM TB_CONSUMABLE" +
                     "WHERE consumable_category = #{consumableCatagory}"
     )
     List<Consumable> findByCategory(String category);
 
-    //가격순으로 조회 하기
+    //가격순으로 조회 하기(높은순)
     @Select(
-            "SELECT * FROM TB_CONSUMABLE\n" +
-                    "WHERE consumable_price\n"+
-                    "ORDER BY DESC"
+            "SELECT \n" +
+                    "consumable_idx,consumable_name,consumable_price,consumable_info,consumable_category,consumable_img \n" +
+                    "FROM TB_CONSUMABLE\n"+
+                    "ORDER BY consumable_price DESC"
     )
-    List<Consumable> priceList();
+    List<Consumable> priceHighList();
 
-    //찜갯수 별로 보여주기
+    //가격순으로 조회 하기(낮은순)
+    @Select(
+            "SELECT \n" +
+                    "consumable_idx,consumable_name,consumable_price,consumable_info,consumable_category,consumable_img \n" +
+                    "FROM TB_CONSUMABLE\n"+
+                    "ORDER BY consumable_price ASC"
+    )
+    List<Consumable> priceLowList();
+
+
+    //찜갯수(인기) 별로 보여주기
     @Select(
             "SELECT * FROM TB_CONSUMABLE\n" +
                     "WHERE consumable_wish = \n" +
